cc53926a5e31e19f3912419bf46e81c8a5492b21
AQVED <yehor.muzychko@gmail.com>
2026-05-28 19:04:30 +0300

    New files with code

---
+++ effects.py
@@ -0,0 +1,9 @@
+from settings import WHITE, GREY, BLACK
+from pygame import draw
+
+def draw_key_effect(screen, rect, is_pressed=False):
+    base_color = GREY if not is_pressed else (170,220, 255)
+    border_color = BLACK
+
+    draw.rect(screen, base_color, rect, border_radius=8)
+    draw.rect(screen, border_color, rect, 2, border_radius=8)
